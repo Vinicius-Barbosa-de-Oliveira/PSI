45d8fe49bb168b84576c48ac1f029daac9465113
Vinicius Barbosa de Oliveira <barbosa.v@academico.ifrn.edu.br>
2023-06-05 15:58:26 -0300

    Aula dia 05 06 2023
    criando produto

--- WebAppProjetoB2023/Context/EFContext.cs
+++ WebAppProjetoB2023/Context/EFContext.cs
@@ -8,8 +8,14 @@ namespace WebAppProjetoB2023.Models
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Asp_Net_MVC_CS") { } // Contrutor que inicializa a classe, chama o metodo construtor da classe pai, que recebe a string de conexão
+        //public EFContext() : base("Asp_Net_MVC_CS") { } // Contrutor que inicializa a classe, chama o metodo construtor da classe pai, que recebe a string de conexão
+        public EFContext() : base("Asp_Net_MVC_CS") // Contrutor que inicializa a classe, chama o metodo construtor da classe pai, que recebe a string de conexão
+                                                    // "base" referencia o metodo contrutor da classe pai(dbcontext)
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>()); //construtor que apaga e recria se o modelo mudou        
+        }
         public DbSet<Categoria> Categorias { get; set; } // faz uma tabela com base na classe Categoria
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
---
+++ WebAppProjetoB2023/Controllers/ProdutoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjetoB2023.Models;
+using System.Data.Entity;
+using System.Net;
+
+namespace WebAppProjetoB2023.Controllers
+{
+    public class ProdutoController : Controller
+    {
+          
+            EFContext context = new EFContext();
+            public ActionResult Index()
+            {
+                var produtos = context.Produtos.Include(c => c.Categoria).Include(f => f.Fabricante).OrderBy(n => n.Nome);
+                //return View(context.Produtos.OrderBy(p => p.Nome));
+                return View(produtos);
+            }
+            
+    }
+}
